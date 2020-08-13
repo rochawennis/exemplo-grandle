83bea88d80a7814cd4a67ac35cf2c94a7f478de7
Wennis <wennisrocha@gmail.com>
2020-08-12 22:33:23 -0300

    grandle-java

---
+++ src/test/java/br/com/ok/exemplogradle/ExemploGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ok.exemplogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
